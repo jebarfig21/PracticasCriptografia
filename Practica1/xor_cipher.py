7cce7b32a27b2ef2450f83af07f84d3b057a567f
jesus Barajas <jesusbarajas@localhost.localdomain>
2020-02-08 22:12:30 -0600

    XOR hecho por Manuel, Practica 1

--- Practica1/xor_cipher.py
+++ Practica1/xor_cipher.py
@@ -5,13 +5,19 @@ def cipher(message):
     Parámetro:
        message -- el mensaje a cifrar.
     """
-    pass
+    # Recorremos cada letra del mensaje, con la operacion ord() obtenemos un integer que representa al caracter en Unicode y finalmente hacemos un xor con la
+    #operacion ^ y el 1 para cambiar en bit los 0's a 1's y los 1's a 0's
+    cifrado = "";
+    for i in range(len(message)): 
+        cifrado = (cifrado + chr(ord(message[i]) ^ 1));
+    return cifrado; 
 
+"""
+Descifra el mensaje recuperando el texto plano siempre y cuando haya sido cifrado con XOR.
+Parámetro:
+cryptotext -- el mensaje a descifrar.
+"""
 def decipher(criptotext):
-    """
-    Descifra el mensaje recuperando el texto plano siempre y cuando haya
-    sido cifrado con XOR.
-    Parámetro:
-       cryptotext -- el mensaje a descifrar.
-    """
-    pass
+    # Aplicamos la misma operacion que utiizamos para cifrar y asi revertir los 0's a 1's y los 1's a 0's
+    return cipher(criptotext); 
+   
